38459cbe743ec42f2e17bae08d74225b220e62cc
Cainã Costa <cainan.costa@gmail.com>
2011-07-13 21:45:56 -0300

    Added a test to generate and publish the report.

--- lib/rack/publish-exceptions.rb
+++ lib/rack/publish-exceptions.rb
@@ -2,6 +2,31 @@ require 'json'
 
 module Rack
   class PublishExceptions
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      status, headers, body =
+        begin
+          @app.call(env)
+        rescue Exception => error
+          create_report(error, env)
+          raise error
+        end
+      [status, headers, body]
+    end
+
+    private
+    def create_report(exception, env)
+      @@publisher.publish({
+        :exception => {
+          :class => exception.class.to_s,
+          :message => exception.to_s,
+        }
+      }.to_json)
+    end
+
     class << self
       def configure
         yield self
--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 describe 'Exception Notification' do
+  it "should generate the report correctly" do
+    app = proc { raise ArgumentError }
+    middleware = Rack::PublishExceptions.new(app)
+
+    assert_raises(ArgumentError) do
+      mock(Rack::PublishExceptions.publisher).publish("{\"exception\":{\"class\":\"ArgumentError\",\"message\":\"ArgumentError\"}}") { "{}" }
+      middleware.call([])
+    end
+  end
 end
