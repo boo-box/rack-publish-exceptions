91e46e8bd1912f80450d44957be90bc1a394d7fe
Cainã Costa <cainan.costa@gmail.com>
2011-07-14 14:52:43 -0300

    Fixed some tests under 1.9.2.

--- test/integration/middleware_test.rb
+++ test/integration/middleware_test.rb
@@ -1,6 +1,19 @@
 require 'integration_helper'
 
 describe 'Middleware' do
+  before do
+    Rack::PublishExceptions.reset_config!
+
+    Rack::PublishExceptions.configure do |c|
+      c.format = proc do |exception, env|
+        {:exception => {
+           :class => exception.class.to_s,
+           :message => exception.message
+        }}.to_json
+      end
+    end
+  end
+
   describe 'if the app raises no error' do
     it "should do nothing if the app raises no error" do
       get '/'
@@ -22,6 +35,17 @@ describe 'Middleware' do
   it "should publish the error if the app raises it" do
     assert_raises(Exception) do
       mock(Rack::PublishExceptions.publisher).publish("{\"exception\":{\"class\":\"Exception\",\"message\":\"Application Error!\"}}") { "{}" }
+
+      get '/error'
+    end
+  end
+
+  it "should publish the errot with a custom format" do
+    assert_raises(Exception) do
+      Rack::PublishExceptions.format = proc do |exception, env|
+        exception.class.to_s
+      end
+      mock(Rack::PublishExceptions.publisher).publish("Exception") { "{}" }
 
       get '/error'
     end
--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -2,6 +2,15 @@ require 'test_helper'
 
 describe 'Exception Notification' do
   it "should generate the report correctly" do
+    Rack::PublishExceptions.configure do |c|
+      c.format = proc do |exception, env|
+        {:exception => {
+           :class => exception.class.to_s,
+           :message => exception.message
+        }}.to_json
+      end
+    end
+
     app = proc { raise ArgumentError }
     middleware = Rack::PublishExceptions.new(app)
 
