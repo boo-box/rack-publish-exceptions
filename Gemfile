66ef4fae34b89390314e08163d22dbe5fa0df5a8
Cainã Costa <cainan.costa@gmail.com>
2011-07-13 21:43:42 -0300

    Added rr as mocking framework.

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ gemspec
 
 group :test do
   gem 'minitest', :require => 'minitest/autorun'
+  gem 'rr', :git => 'git://github.com/btakita/rr.git'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,9 @@ Bundler.require :test
 require 'minitest/spec'
 require 'minitest/pride'
 
+class MiniTest::Unit::TestCase
+  include RR::Adapters::MiniTest
+end
+
 $:.push 'lib'
 require 'rack/publish-exceptions'
